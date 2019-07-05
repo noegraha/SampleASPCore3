eaf71d6a974222b4e99dcdac643d88bb47860326
nugraha.triatmaja@gmail.com <nugraha.triatmaja@gmail.com>
2019-07-05 14:06:54 +0700

    First Commit

---
+++ Controllers/RegistrasiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using Microsoft.AspNetCore.Mvc;
+using SampleASPCore.Models;
+
+namespace SampleASPCore2.Controllers
+{
+
+    public class RegistrasiController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Tampil(Anggota anggota)
+        {
+
+            //return Content($"Nama : {anggota.Firstname} {anggota.Lastname} {anggota.Address} {anggota.Telp}");
+            return View(anggota);
+        }
+
+    }
+}
---
+++ Models/Anggota.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleASPCore.Models
+{
+    public class Anggota
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Telp { get; set;  }
+    }
+}
